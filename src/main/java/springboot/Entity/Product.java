056f66db64a1461cee7bfe2ec5305fa0b6682546
talel <talelmejri8@gmail.com>
2022-08-10 16:32:12 +0100

    first

---
+++ src/main/java/springboot/Entity/Product.java
@@ -0,0 +1,46 @@
+package springboot.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="produit")
+public class Product {
+	@Id 
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	private long productid;
+	private String libelle;
+	private float prix;
+	
+	public Product() {
+		
+	}
+
+	public long getProductid() {
+		return productid;
+	}
+
+	public void setProductid(long productid) {
+		this.productid = productid;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public float getPrix() {
+		return prix;
+	}
+
+	public void setPrix(float prix) {
+		this.prix = prix;
+	}
+	
+}
---
+++ src/test/java/springboot/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
