ecce55be05513d8e7ab3aa967cf5d1c83c70277f
talel <talelmejri8@gmail.com>
2022-08-15 19:20:55 +0100

    validation

--- src/main/java/springboot/service/ProductService.java
+++ src/main/java/springboot/service/ProductService.java
@@ -2,9 +2,8 @@ package springboot.service;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import springboot.Entity.Product;
+import springboot.Exception.ProductException;
 import springboot.Repository.ProductRepository;
 
 @Service
@@ -13,9 +12,25 @@ public class ProductService {
 	@Autowired 
 	ProductRepository myfriend;
 	
-	public Product addProduct(Product p) {
+	public Product addProduct(Product p) throws ProductException {
+		if(p.getLibelle().isEmpty()) {
+		
+			throw new ProductException("libelle reqiured");
+		}
+		//System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1");
+		 if(p.getPrix()<=0) {
+		
+			throw new ProductException("prix invalid");
+		}
+			//System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa2");
+		//System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa0"+myfriend.findByLibelle(p.getLibelle()));
+		if(myfriend.findByLibelle(p.getLibelle())!=null) {
+			throw new ProductException("libelle déja existe");
+		}
+		//System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa3");
 		p = myfriend.save(p);//tsob fil base de donneé baad trajaa 
-		return p;
+		//System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa4");
+			return p;
 	}
 
 	public Product updateProduct(long ProductId,Product p) {
@@ -37,4 +52,5 @@ public class ProductService {
 	public void deleteProduit(long ProductId) {
 		myfriend.deleteById(ProductId);
 	}
+	
 }
